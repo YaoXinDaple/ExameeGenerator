ec122b1e5369891cfc07da7603bdcfd99916ef3f
YaoXinDaple <dapleyx@gmail.com>
2026-03-30 23:28:34 +0800

    add dbcontext injection

--- src/ExameeGenerator.Infrastructure/DependencyInjection.cs
+++ src/ExameeGenerator.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,20 @@
 ﻿using ExameeGenerator.Application.Interfaces;
 using ExameeGenerator.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ExameeGenerator.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
         {
             services.AddScoped<IExamRepository, ExamRepository>();
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("examdb")));
 
             return services;
         }
